764f4db464f0473110e908da53f47d7e431da31f
fareesnazmi <fareesnazmi2@gmail.com>
2021-09-22 23:31:59 +0800

    Edit&Delete (users&suppliers)- Super Admin Page
    Super Admin able to edit and delete for registration users as well as registration suppliers.
    Folder changed "mainadmin" to "superadmin"

---
+++ superadmin/includes/leftbar.php
@@ -0,0 +1,26 @@
+	<nav class="ts-sidebar">
+			<ul class="ts-sidebar-menu">
+
+				<li class="ts-label">Main Board</li>
+				<li><a href="dashboard.php"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+
+<ul>
+</ul>
+</li>
+
+<li><a href="#"><i class="fa fa-sitemap"></i> Registration</a>
+					<ul>
+						<li><a href="reg-users.php">Registration Users</a></li>
+						<li><a href="reg-suppliers.php">Registration Suppliers</a></li>
+					</ul>
+</li>
+				<li><a href="manage-bookings.php"><i class="fa fa-users"></i> Product</a></li>
+				<li><a href=""><i class="fa fa-users"></i> Memberships</a></li>
+				<li><a href=""><i class="fa fa-users"></i> Courier</a></li>
+				<li><a href=""><i class="fa fa-users"></i> Brands</a></li>
+				<li><a href="manage-contactusquery.php"><i class="fa fa-desktop"></i> Forum</a></li>
+
+			<!-- <li><a href="manage-subscribers.php"><i class="fa fa-table"></i> Manage Subscribers</a></li> -->
+
+			</ul>
+		</nav>
