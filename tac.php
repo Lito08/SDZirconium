e69ee7da7cc6ec5cc34fc51a7a9941243279527e
adamafiq345 <adamafiq345@gmail.com>
2021-09-28 15:58:14 +0800

    update tac

--- tac.php
+++ tac.php
@@ -73,20 +73,39 @@ $(document).ready(function() {
 
 <header class="section-heading">
 <h2 class="section-title">Introduction</h2>
-<p class="lead">Life is easy why make it hard?</p>
+<p class="lead"> Welcome to the zirconium.com.my website. These Terms of Use govern your access and use of the Platform and the use of any services, information and functions made available by us at the Platform (“Services”). Before using this Platform or the Services, you must read carefully and accept these Terms of Use and all other terms and conditions and policies pertaining to the use of the Platform and/or the Services (collectively referred to as “zirconium Terms and Conditions”) and you must consent to the processing of your personal data. By accessing the Platform and/or using the Services, you agree to be bound by zirconium Terms and Conditions and any amendments to the foregoing issued by us from time to time. If you do not agree to zirconium Terms and Conditions and the Privacy Policy, do not access and/or use this Platform and/or the Services</p>
 <br><br>
 <h2 class="section-title">Voucher</h2>
-<p class="lead">Life is easy why make it hard?</p>
+<p class="lead">1. A valid code must be entered at the checkout page in order to redeem a Voucher.</p>
+<p class="lead">3. Customers can only redeem up to one Voucher per day.</p>
+
+<p class="lead">4. Customers can only use the Voucher for a one-time purchase only.</p>
+
+<p class="lead">5. Voucher validity is limited in time and such validity period shall be as prescribed in any individual voucher policy</p>
 <br><br>
 <h2 class="section-title">Bills Payment</h2>
-<p class="lead">Life is easy why make it hard?</p>
+<p class="lead">1. The Customer acknowledges that the Bills Payment Services are provided and managed by Third Party Billers. Accordingly, the bill payment transactions and agreements are entered directly between the Customer and Third Party Billers</p>
+<p class="lead">2. Bills payments will be made by the Third Party Billers according to the information provided by the Customer. The Customer shall be responsible for any errors in any information given, such as but not limited to bill details, account name, and amounts. The Customer shall check carefully before payment, as the transactions are non-cancellable and non-refundable once payments are made.</p>
+<p class="lead">3.  By availing of the Bills Payment Services and providing their personal information, the Customer consents to the transmission, transfer or processing of said information as may be deemed necessary or appropriate to perform the bills payment services referenced herein. The Customer likewise acknowledges and agrees to zirconium’s Privacy Policy as applicable from time to time</p>
 <br><br>
 <h2 class="section-title">Zirconium Payment</h2>
-<p class="lead">Life is easy why make it hard?</p>
+<p class="lead">1.1 Protection against Unauthorized Transactions.
+
+Zirconium will provide you with compensation for Unauthorized Transactions (defined below), provided that you comply with the procedures set out within the Policy.
+
+An "Unauthorized Transaction" occurs when, because of a fraudulent take-over of the zirconium Account:
+<br><br>
+(a) transaction is made from the value stored within your zirconium Wallet that you did not authorize and that did not benefit you;
+<br><br>
+(b) a transaction or Top Up to the zirconium wallet is made from a credit or debit card saved in your zircoinum Account that you did not authorise and that did not benefit you; or
+<br><br>
+(c) a transaction or Top Up to the zirconium wallet is made from the Direct Link account, that you did not authorise and that did not benefit you.
+<br><br>
+Notwithstanding the foregoing, you have an obligation to ensure the safety and security of your zirconium Account. If you authorize someone to access your Wallet by providing them with your login information or otherwise, and they conduct transactions without your knowledge or permission, you shall be responsible for all consequences arising from such use.</p>
 <br><br>
 <h2 class="section-title">Return Policy</h2>
-<p class="lead">Life is easy why make it hard?</p>
-
+<p class="lead"><b>7 days or 14 days return</p>
+<p class="lead">A return to seller policy is when sellers will either approve or reject request base on valid return reason and additional feedback given by customer.As part of the process, you may initiate a chat for discussion on the return request.The return validaty period start when you receive the item. </p>
 </header><!-- sect-heading -->
 
 
